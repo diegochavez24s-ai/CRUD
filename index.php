f94dd6a9c4c1ffae19494e39f5fd56ba58ce7bba
diegochavez24s-ai <diego.chavez.24s@utzmg.edu.mx>
2025-10-27 22:54:53 -0600

    cree la tabla para a la hora de ingresar datos se muestren hay en la tablita :)

--- index.php
+++ index.php
@@ -10,28 +10,33 @@
     <form class="col-4">
   <div class="form-group">
     <label for="exampleFormControlInput1">Nombre</label>
-    <input type="Text" class="form-control" id="exampleFormControlInput1" placeholder="Name">
+    <input type="text" class="form-control" placeholder="Name">
   </div>
-   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-    </div>
+   <div class="form-group">
+    <label for="exampleFormControlInput1">Apellidos de la persona</label>
+    <input type="text" class="form-control" placeholder="Apellido">
+  </div>
     <div class="form-group">
-    <label for="exampleFormControlSelect1">Genero</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>Msculino</option>
-      <option>Femenino</option>
-      <option>Otro</option>
-    </select>
+    <label for="exampleFormControlInput1">Numero de la persona</label>
+    <input type="text" class="form-control" placeholder="TELEFONO">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Correo</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     <div class="col-8 p-4">
 <table class="table table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">NOMBRE</th>
+      <th scope="col">APELLIDOS</th>
+      <th scope="col">NUMERO</th>
+       <th scope="col">CORREO</th>
+       <th scope="col">ACCION</th>
     </tr>
   </thead>
   <tbody>
@@ -39,20 +44,15 @@
       <th scope="row">1</th>
       <td>Mark</td>
       <td>Otto</td>
+      <td>3323299878</td>
       <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+        <td>
+        <a href="editar.php?id=1">Editar</a> 
+        <a href="eliminar.php?id=1">Eliminar</a> 
+    </td>
+</tr>
+       </td>  
+
   </tbody>
 </table>
   </div>
