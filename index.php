80562fe78bab989d95f8844d2fc9cfbe05afa240
diegochavez24s-ai <diego.chavez.24s@utzmg.edu.mx>
2025-10-27 19:51:02 -0600

    agregue la tabla paro falta modificarla

--- index.php
+++ index.php
@@ -9,14 +9,53 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <form class="col-4">
   <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <label for="exampleFormControlInput1">Nombre</label>
+    <input type="Text" class="form-control" id="exampleFormControlInput1" placeholder="Name">
   </div>
    <div class="form-group row">
     <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
     <div class="col-sm-10">
       <input type="password" class="form-control" id="inputPassword" placeholder="Password">
     </div>
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">Genero</label>
+    <select class="form-control" id="exampleFormControlSelect1">
+      <option>Msculino</option>
+      <option>Femenino</option>
+      <option>Otro</option>
+    </select>
+    <div class="col-8 p-4">
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
 </body>
 </html>
 
